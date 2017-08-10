7a96ce6e47801f0dca16858943459ff3910ff3c4
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-10 13:16:41 +0400

    added lists for skidays

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount    total={50}
-                    powder={20}
-                    backcountry={10}
-                    goal={100} />,
+	<SkiDayList days={
+        [
+            {
+                resort: "Safari Mountain",
+                date: new Date("2/8/2017"),
+                powder: true,
+                backcountry: false
+            },
+            {
+                resort: "Baja California",
+                date: new Date("6/10/2017"),
+                powder: false,
+                backcountry: true
+            },
+            {
+                resort: "Victoria Waterfall",
+                date: new Date("11/8/2017"),
+                powder: false,
+                backcountry: false
+            }
+        ]
+    } />,
 	document.getElementById('react-container')
 )
