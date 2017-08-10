43b4a68ae283eb804212880cb6ac65440df2f961
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-10 11:49:53 +0400

    Initial commit, working with current react version

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: __dirname + "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loaders: ["babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=react"]
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
+}
+
+
+
+
+
+
+
