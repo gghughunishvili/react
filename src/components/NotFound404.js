4dd5bb88f6d58fb019abb22931b4cb7b4f19f620
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-11 12:34:02 +0400

    added react router and menu, added simple filter for skidays

---
+++ src/components/NotFound404.js
@@ -0,0 +1,10 @@
+import { Component } from  'react'
+export class NotFound404 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Route Not Found!</h1>
+            </div>
+        )
+    }
+}
