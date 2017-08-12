c5d188bb508ec7cec9c0e60f714ace8d7487fa81
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-12 20:05:06 +0400

    added action creators implementation

--- activity-counter-redux/src/index.js
+++ activity-counter-redux/src/index.js
@@ -1,46 +1,16 @@
-import C from "./constants"
 import storeFactory from "./store"
+import { addDay, removeDay, setGoal } from './actions'
 
+const store = storeFactory()
 
-const initialState = (localStorage['redux-store']) ?
-    JSON.parse(localStorage['redux-store']) :
-    {}
+store.dispatch(
+    addDay("Heavenly", "2016-12-22")
+)
 
-const saveState = () => {
-    const state = JSON.stringify(store.getState())
-  localStorage['redux-store'] = state
-}
+store.dispatch(
+    removeDay("2016-12-22")
+)
 
-const store = storeFactory(initialState)
-
-store.subscribe(saveState)
-
-store.dispatch({
-    type: C.ADD_DAY,
-    payload: {
-        "resort": "Mt Shasta",
-        "date": "2016-10-28",
-        "powder": true,
-        "backcountry": true
-    }
-})
-
-store.dispatch({
-    type: C.ADD_DAY,
-    payload: {
-        "resort": "Squaw Valley",
-        "date": "2016-3-28",
-        "powder": true,
-        "backcountry": false
-    }
-})
-
-store.dispatch({
-    type: C.ADD_DAY,
-    payload: {
-        "resort": "The Canyons",
-        "date": "2016-1-2",
-        "powder": false,
-        "backcountry": true
-    }
-})
+store.dispatch(
+  setGoal(55)
+)
