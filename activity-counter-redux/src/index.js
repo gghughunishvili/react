f67f50f52fbfbfefa80c828f5b3403af00383746
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-12 18:53:24 +0400

    added webpack config and getting ready for store

--- activity-counter-redux/src/index.js
+++ activity-counter-redux/src/index.js
@@ -1,41 +1,4 @@
 import C from "./constants"
-import { skiDay, errors } from "./store/reducers"
-
-const state = null
-
-const errorState = [
-    "user not authorized",
-    "wrong type of variable has been passed"
-]
-
-const action = {
-    "type": C.ADD_DAY,
-    "payload": {
-        "resort": "Some Resort",
-        "date": "2017-11-11",
-        "powder": false,
-        "backcountry": true
-    },
-}
-
-const nextState = skiDay(state, action)
-
-console.log(nextState)
-
-const errorAction = {
-    "type": C.ADD_ERROR,
-    "payload": "Cannot connect to server",
-}
-
-const errorNextState = errors(errorState, errorAction)
-
-console.log(errorNextState)
-
-const errorClearAction = {
-    "type": C.CLEAR_ERROR,
-    "payload": 0,
-}
-
-const errorClearNextState = errors(errorState, errorClearAction)
-
-console.log(errorClearNextState)
+import appReducer from "./store/reducers"
+import initialState from './initialState.json'
+import { createStore } from 'redux'
--- activity-counter-redux/src/store/reducers.js
+++ activity-counter-redux/src/store/reducers.js
@@ -1,4 +1,5 @@
 import C from "../constants"
+import { combineReducers } from 'redux'
 
 export const goal = (state=10, action) => {
     if (action.type === C.SET_GOAL) {
@@ -16,11 +17,11 @@ export const skiDay = (state=null, action) => {
 
 export const errors = (state=[], action) => {
     switch(action.type) {
-        case C.ADD_ERROR:
+        case C.ADD_ERROR :
             return [...state, action.payload]
-        case C.CLEAR_ERROR:
+        case C.CLEAR_ERROR :
             return state.filter((messate, i) => i !== action.payload)
-        default:
+        default :
             return state
     }
     if (action.type === C.ADD_DAY) {
@@ -28,3 +29,59 @@ export const errors = (state=[], action) => {
     }
     return state
 }
+
+export const allSkiDays = (state=[], action) => {
+    switch(action.type) {
+        case C.ADD_DAY : 
+            const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
+            return (hasDay) ?
+                state :
+                [
+                    ...state,
+                    skiDay(null, action)
+                ].sort((a, b) => new Date(b.date) - new Date(a.date))
+
+        case C.REMOVE_DAY :
+            return state.filter(skiDay => skiDay.date !== action.payload)     
+
+        default:
+            return state
+    }
+}
+
+export const fetching = (state=false, action) => {
+    switch(action.type) {
+        case C.FETCH_RESORT_NAMES :
+            return true
+
+        case C.CANCEL_FETCHING :
+            return false 
+
+        case C.CHANGE_SUGGESTIONS :
+            return false   
+
+        default:
+            return state
+    }
+}
+
+export const suggestions = (state=[], action) => {
+    switch(action.type) {
+        case C.CLEAR_SUGGESTIONS :
+            return []
+        case C.CHANGE_SUGGESTIONS :
+            return action.payload  
+        default :
+            return state
+    }
+}
+
+export default combineReducers({
+    goal,
+    skiDay,
+    errors,
+    resortNames: combineReducers({
+        fetching,
+        suggestions
+    })
+})
