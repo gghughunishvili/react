c1296f331b7bcdd5370502e7dbebfc1006c38c1a
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-16 17:51:52 +0400

    removed old stuff, started with new architecture

---
+++ weather-app/src/containers/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default SearchBar extends Component {
+    render () {
+        return (
+            <form className="input-group">
+
+            </form>
+        )
+    }
+}
