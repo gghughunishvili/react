c935b6b3f8d9fe4ff323a88826aa4bb7b7d80544
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-17 14:55:24 +0400

    added styling and node-sass

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import SearchBar from '../containers/SearchBar'
 import WeatherList from '../containers/WeatherList'
+import '../stylesheets/index.scss'
 
 export default class App extends Component {
 
