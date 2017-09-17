9de2809eb394fb00b85752d7e2ab136a80ac25f2
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-17 13:56:55 +0400

    removed reading from api-keys and added static key passing

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-import ApiKeys from '../../api-keys'
-const API_KEY = ApiKeys.openweathermap
+const API_KEY = '39f5a2d96c32b68908c66e0f04d0047e'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
