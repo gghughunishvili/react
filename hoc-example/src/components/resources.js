27e458201452eaa77920ce4969e32565bfde6480
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-24 11:19:21 +0400

    added final reducers, actions and hoc that requires auth

---
+++ hoc-example/src/components/resources.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from './header'
+
+export default class Resources extends Component {
+  render() {
+    return (
+      <div>
+          I am a resource you dumb!
+      </div>
+    )
+  }
+}
