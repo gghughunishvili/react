6b409430c382f18c0b0d8fc94b2ce2de12f9af94
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-24 23:45:42 +0400

    added signin, signout and signup

--- token-auth-example/src/actions/index.js
+++ token-auth-example/src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { browserHistory } from 'react-router'
+import { AUTH_USER, AUTH_ERROR, DEAUTH_USER } from './types'
+
 const config = require('../../config.js')
 
 
@@ -18,10 +20,36 @@ export function signinUser (email, password) {
     return function (dispatch) {
         axios.post(`${ROOT_URL}/oauth/token`, sendData)
             .then(response => {
-
+                dispatch({ type: AUTH_USER })
+                localStorage.setItem('access_token', response.data.access_token)
+                browserHistory.push('/features')
             })
             .catch(() => {
-
+                dispatch(authError('Bad login info!'))
             })
     }
+}
+
+export function signupUser ({ email, password, password_confirmation }) {
+    return function (dispatch) {
+        axios.post(`${ROOT_URL}/users`, {email, password, password_confirmation})
+            .then(response => {
+                dispatch({ type: AUTH_USER })
+                browserHistory.push('/features')
+            })
+            .catch(error => dispatch(authError(error.response.data)))
+    }
+}
+
+export function authError (error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
+}
+
+export function signoutUser () {
+    localStorage.removeItem('access_token')
+
+    return {type: DEAUTH_USER}
 }
