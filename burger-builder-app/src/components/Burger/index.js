264a8acd679f2bd76bcf308984b6081bb8d9831c
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-01-17 17:07:34 +0400

    added displaying info when no ingredients are set

--- burger-builder-app/src/components/Burger/index.js
+++ burger-builder-app/src/components/Burger/index.js
@@ -5,12 +5,17 @@ import classes from './style.css'
 
 class Burger extends Component {
     render() {
-        const transformedIngredients = Object.keys(this.props.ingredients)
+        let transformedIngredients = Object.keys(this.props.ingredients)
             .map(ingrKey => {
                 return [...Array(this.props.ingredients[ingrKey])].map((_, i) => {
                     return <BurgerIngredient key={ingrKey + 1} type={ingrKey}/>
                 })
-            })
+            }).reduce((arr, el) => {
+                return arr.concat(el)
+            }, [])
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients</p>
+        } 
         return (
             <div className={classes.burger}>
                 <BurgerIngredient type="bread-top" />
